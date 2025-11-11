ebc80d5f7c244b33708b11c604f6f38ed5ca898c
Thyago Medeiros de Oliveira <thyago.m.oliveira@bradesco.com.br>
2025-11-11 17:44:33 +0000

    Spring Exercício 0

---
+++ 0x01/spring_boot_1/demo/src/main/java/com/example/demo/MessageResource.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/messages")
+public class MessageResource {
+	
+	@GetMapping("/simpleMessageWelcome")
+    public String simpleMessageWelcome() {
+        return "BEM VINDO A AULA DE MICROSSERVIÇO USANDO SPRING BOOT !!!";
+    }
+	
+	@GetMapping("/login/{user}/{password}")
+	public String login(@PathVariable String user, @PathVariable String password) {
+		
+		if ((user == null && user.equals("")) && (password == null && password.equals(""))) {
+			return "USUÁRIO E SENHA NÃO INFORMADOS";
+		}
+		
+		if (user.length() > 15 || password.length() > 15) {
+			return "USUÁRIO E SENHA INVÁLIDOS";
+		}
+		
+		return "LOGIN EFETUADO COM SUCESSO !!!";
+		
+	}
+}
