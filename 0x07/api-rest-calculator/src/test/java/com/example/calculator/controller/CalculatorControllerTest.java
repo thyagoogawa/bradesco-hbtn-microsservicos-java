65e3f8ca163e29d5d8abdb36c91f57118012747f
Thyago Medeiros de Oliveira <thyago.m.oliveira@bradesco.com.br>
2025-12-14 15:07:55 +0000

    Ajuste Checker

--- 0x07/api-rest-calculator/src/test/java/com/example/calculator/controller/CalculatorControllerTest.java
+++ 0x07/api-rest-calculator/src/test/java/com/example/calculator/controller/CalculatorControllerTest.java
@@ -4,15 +4,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 
 import com.example.calculator.model.Calculator;
 
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(CalculatorController.class)
 // Annotation @Import necessário para disponibilizar o bean no teste
 @Import(Calculator.class)
